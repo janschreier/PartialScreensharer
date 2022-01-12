d485fcaaa8fe866152b1cf302626ea45097cdecc
Jan Schreier <jan.schreier@posteo.de>
2022-01-12 07:54:10 +0100

    Enabled different skype usage

--- PartialScreensharerApp/MainWindow.xaml.cs
+++ PartialScreensharerApp/MainWindow.xaml.cs
@@ -5,72 +5,85 @@ using System.Windows;
 using System.Windows.Media.Imaging;
 using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
-namespace PartialScreensharerApp
+namespace PartialScreensharerApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private readonly System.Windows.Media.ImageSource _originalImage;
+    private int? _height;
+    private int _width;
+    private int _imageLeftPos;
+    private int _imageTopPos;
+
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-            Height = SystemParameters.WorkArea.Height;
-            Width = SystemParameters.WorkArea.Width / 2;
-
-            Top = SystemParameters.WorkArea.Top;
-            Left = SystemParameters.WorkArea.Left;
-            _ = RunUpdateImageTask();
-        }
-
-
-        private async Task RunUpdateImageTask()
-        {
-            while (true)
-            {
-                await Task.Delay(200);
-
-                if (!IsActive)
-                {
-                    WarningText.Visibility = Visibility.Collapsed;
-                    UpdateImage();
-                }
-                else
-                {
-                    WarningText.Visibility = Visibility.Visible;
-                }
-            }
-        }
-
-        private void UpdateImage()
-        {
-            var height = (int)(Image.ActualHeight == 0 ? Height : Image.ActualHeight);
-            var width = (int)(Image.ActualWidth == 0 ? Width : Image.ActualWidth);
-            var imageLeftPos = (int)(Left + (Width - width) / 2);
-            var imageTopPos = (int)(Top + (Height - height) / 2);
-
-            using var bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-            using var graphics = Graphics.FromImage(bitmap);
-            graphics.CopyFromScreen(imageLeftPos, imageTopPos, 0, 0, bitmap.Size, CopyPixelOperation.SourceCopy);
-            Image.Source = BitmapToImageSource(bitmap);
-        }
-
-
-        private static BitmapImage BitmapToImageSource(Image bitmap)
-        {
-            using var memory = new MemoryStream();
-            bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
-            memory.Position = 0;
-            var bitmapimage = new BitmapImage();
-            bitmapimage.BeginInit();
-            bitmapimage.StreamSource = memory;
-            bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
-            bitmapimage.EndInit();
-            return bitmapimage;
-        }
-
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+        InitializeComponent();
+        Height = SystemParameters.WorkArea.Height;
+        Width = SystemParameters.WorkArea.Width / 2;
+        Top = SystemParameters.WorkArea.Top;
+        Left = SystemParameters.WorkArea.Left;
+        _originalImage = Image.Source;
+        _ = RunUpdateImageTask();
     }
 
+    private async Task RunUpdateImageTask()
+    {
+        while (true)
+        {
+            await Task.Delay(200);
 
+            if (!IsActive)
+            {
+                WarningText.Visibility = Visibility.Collapsed;
+                UpdateImage();
+            }
+            else
+            {
+                WarningText.Visibility = Visibility.Visible;
+                Image.Source = _originalImage;
+            }
+        }
+    }
+
+    private void UpdateImage()
+    {
+        if (_height == null)
+        {
+            SetArea();
+        }
+        using var bitmap = new Bitmap(_width, _height!.Value, PixelFormat.Format32bppArgb);
+        using var graphics = Graphics.FromImage(bitmap);
+        graphics.CopyFromScreen(_imageLeftPos, _imageTopPos, 0, 0, bitmap.Size, CopyPixelOperation.SourceCopy);
+        Image.Source = BitmapToImageSource(bitmap);
+    }
+
+    private void SetArea()
+    {
+        _height = (int)(Image.ActualHeight == 0 ? Height : Image.ActualHeight);
+        _width = (int)(Image.ActualWidth == 0 ? Width : Image.ActualWidth);
+        _imageLeftPos = (int)(Left + (Width - _width) / 2);
+        _imageTopPos = (int)(Top + (Height - _height) / 2);
+        SetAreaButton.Content = $"Set Area (currently defined area: {_imageLeftPos}, {_imageTopPos}, {_width}, {_height} )";
+    }
+
+    private static BitmapImage BitmapToImageSource(Image bitmap)
+    {
+        using var memory = new MemoryStream();
+        bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+        memory.Position = 0;
+        var bitmapimage = new BitmapImage();
+        bitmapimage.BeginInit();
+        bitmapimage.StreamSource = memory;
+        bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+        bitmapimage.EndInit();
+        return bitmapimage;
+    }
+
+    private void CloseApp(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+    private void SetArea(object sender, RoutedEventArgs e) => SetArea();
+
+  
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PartialScreensharer
+# Partial Screensharer
 
 Have your colleagues complained not being able to see what you shared with them, because your screen is ultra-wide while theirs is smaller? 
 Have you whished to share only a part of the screen? 
@@ -6,13 +6,23 @@ Have you whished to share only a part of the screen?
 ## Requirements
 * Build with .net 6.0.1 and thus requires .net 6.0.1 runtime
 
-
-## Usage:
-1. Start PartialScreensharer
+## Usage with Teams:
+1. Start Partial Screensharer
 2. Position to the area you want others to be able to see
 3. Share the App's window
 4. Move the stuff you want to share over that window and your colleagues will see it.
 5. To stop the app, close the app, either by using the button or by rightclicking in the taskbar.
+
+## Usage with Skype:
+1. Start Partial Screensharer
+2. Position to the area you want others to be able to see
+3. Click "Set Area"
+4. Move the window to an area on your screen that will be visible to yourself during the entire screen sharing session 
+(otherwise no one will see anything as Skype only displays that part of the window you can see yourself wheras Teams shows 
+the windows content)
+5. Share the App's window
+6. Move the stuff you want to share over that window and your colleagues will see it.
+7. To stop the app, close the app, either by using the button or by rightclicking in the taskbar. 
 
 ## Download
 For those seeking for an .exe-File I put one up on my server. [Download it here](https://janschreier.de/PartialScreensharer.zip) or compile the above sourcecode
